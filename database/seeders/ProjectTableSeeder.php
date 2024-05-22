730bd7554bf326fe5cb24ed74b4d8d4441cf97e1
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-22 18:02:16 +0200

    [add] helper slug

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -12,11 +12,11 @@ class ProjectTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = ['Project1', 'Project2', 'Project3', 'Project4', 'Project5'];
+        $data = ['Project1', 'Project2', 'Project3', 'Project4', 'Project5', 'Project6', 'Project7'];
         foreach ($data as $item) {
             $new_item = new Project();
             $new_item->title = $item;
-            $new_item->slug = ;
+            $new_item->slug = Help::generateSlug($item, Project::class);
             $new_item->save();
         }
     }
