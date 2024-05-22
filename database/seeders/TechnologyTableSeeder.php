c44b4e5b9825c8f10956a542eeea42b192d5f96b
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-22 18:40:45 +0200

    [add] technology seeder

--- database/seeders/TechnologyTableSeeder.php
+++ database/seeders/TechnologyTableSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Functions\Helper;
 use Illuminate\Database\Seeder;
+use App\Functions\Helper;
+use App\Models\Technology;
 
 class TechnologyTableSeeder extends Seeder
 {
